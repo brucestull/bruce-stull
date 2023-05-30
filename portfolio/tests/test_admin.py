0a5cf4dbd91229fba96ef1e9a84475c8c3883a5e
Flynnt Knapp <bruce.stull@gmail.com>
2023-05-30 05:46:59 -0400

    Tests are completed for 'portfolio' 'admin.py' and 'models.py', still need to make tests for 'views.py'
    .

--- portfolio/tests/test_admin.py
+++ portfolio/tests/test_admin.py
@@ -25,7 +25,7 @@ class TestTechnologyAdmin(TestCase):
         technology_admin = TechnologyAdmin(Technology, None)
         fieldsets = technology_admin.get_fieldsets(request=None, obj=None)
         fieldsets_as_list = list(fieldsets)
-        self.assertIn("name", fieldsets_as_list[0][1]['fields'][0])
+        self.assertIn("name", fieldsets_as_list[0][1]["fields"][0])
 
     def test_get_fieldsets_has_description_in_second_element(self):
         """
@@ -34,7 +34,7 @@ class TestTechnologyAdmin(TestCase):
         technology_admin = TechnologyAdmin(Technology, None)
         fieldsets = technology_admin.get_fieldsets(request=None, obj=None)
         fieldsets_as_list = list(fieldsets)
-        self.assertIn("description", fieldsets_as_list[0][1]['fields'][1])
+        self.assertIn("description", fieldsets_as_list[0][1]["fields"][1])
 
     def test_get_fieldsets_has_created_at_in_third_element(self):
         """
@@ -43,7 +43,7 @@ class TestTechnologyAdmin(TestCase):
         technology_admin = TechnologyAdmin(Technology, None)
         fieldsets = technology_admin.get_fieldsets(request=None, obj=None)
         fieldsets_as_list = list(fieldsets)
-        self.assertIn("created_at", fieldsets_as_list[0][1]['fields'][2])
+        self.assertIn("created_at", fieldsets_as_list[0][1]["fields"][2])
 
 
 class TestProjectAdmin(TestCase):
@@ -51,32 +51,33 @@ class TestProjectAdmin(TestCase):
     Test the `ProjectAdmin` class.
     """
 
-    # def test_get_fieldsets_is_list_of_tuples(self):
-    #     """
-    #     `ProjectAdmin` `get_fieldsets()` method should return a list of tuples.
-    #     """
-    #     project_admin = ProjectAdmin(Project, None)
-    #     fieldsets = project_admin.get_fieldsets(request=None, obj=None)
-    #     self.assertIsInstance(fieldsets, list)
-    #     self.assertIsInstance(fieldsets[0], tuple)
+    def test_project_admin_truncated_description_method_returns_description(self):
+        """
+        `ProjectAdmin` `truncated_description()` method should return the `description`.
+        """
+        project_admin = ProjectAdmin(Project, None)
+        project = Project.objects.create(
+            title="Test Project",
+            description="Test description",
+            image="test.jpg",
+        )
+        self.assertEqual(
+            project_admin.truncated_description(project),
+            project.description,
+        )
 
-    # def test_get_fieldsets_is_list_of_tuples(self):
-    #     """
-    #     `ProjectAdmin` `get_fieldsets()` method should return a list of tuples.
-    #     """
-    #     def truncated_description(obj):
-    #         """
-    #         Mock the truncated_description method.
-    #         """
-    #         return obj.description
-
-    #     project_admin = ProjectAdmin(Project, None)
-    #     project_admin.truncated_description = truncated_description
-
-    #     fieldsets = project_admin.get_fieldsets(request=None, obj=None)
-    #     self.assertIsInstance(fieldsets, list)
-    #     self.assertIsInstance(fieldsets[0], tuple)
-
-
-
-    pass
+def test_project_admin_truncated_description_method_returns_truncated_description(self):
+        """
+        `ProjectAdmin` `truncated_description()` method should return the truncated `description`.
+        """
+        project_admin = ProjectAdmin(Project, None)
+        project = Project.objects.create(
+            title="Test Project",
+            description="This is a description with more than 30 characters.",
+            image="test.jpg",
+        )
+        truncated_description = "This is a description with more than 30 characters."[:30]
+        self.assertEqual(
+            project_admin.truncated_description(project),
+            truncated_description,
+        )
